c22dffd229cf62df7cc1667bc0cb27f572f8366b
Sharaar <walidsharar@gmail.com>
2021-06-16 12:19:32 +0200

    user choice is updated

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+
+
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like to drink? {options} \nif you don't to proceed type Off " ) .lower()
+    if choice == "off":
+        is_on = False
+    elif choice=="report":
+        coffee_maker.report()
+        money_machine.report()
+
+
+
+
